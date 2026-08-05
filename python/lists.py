974eb10459a76521c27ddbac4f2f56d520bf18b2
fiza Noor <fnoor034726@gmail.com>
2026-08-06 03:29:11 +0500

    practice

---
+++ python/lists.py
@@ -0,0 +1,51 @@
+
+#lists (are mutable can be changed)
+# names = ["fiza", "iqra", "mehak"]
+# print(names[0])
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print(planets[-1]) #accessing the last element 
+print(planets[0:3])#slicing
+print(planets[:-1])
+print(planets[-3:])
+#modifiable
+planets[3] = 'Malacandra'
+planets[:3] =  ['Mur', 'Vee', 'Ur']
+print(planets)
+
+#list functions
+print(len(planets))
+
+# The planets sorted in alphabetical order
+print(sorted(planets))
+
+#sum of list
+primes = [2, 3, 5,[2,3]]
+print(len(primes))
+# print(sum(primes[0]))
+
+#max of list
+# print(max(primes[0]))
+
+#objects in list
+x = 12
+print(x.imag)
+c = 12 + 3j
+real = int(c.real)
+print(help(real.bit_length()))
+
+#list methods
+#list.append modifies a list by adding an item to the end:
+
+planets.append('Pluto')
+print(planets)
+
+#list.pop removes and returns the last element of a list:
+print(planets.pop())
+print(planets)
+
+print(planets.index('Ur'))
+print("pluto" in planets)
+d = [1, 2, 3][1:]
+print(len(d))
+print("Index:", planets.index('Ur'))
--- python/name.py
+++ python/name.py
@@ -11,14 +11,25 @@
 #     print("login again")
 
 
-# n = int(input("Enter a number: "))
+# # n = int(input("Enter a number: "))
 
-n = input("Enter a number: ")
+# n = input("Enter a number: ")
 
-num = int(n);
-if num > 0:
-    print("Number is positive")
-elif num < 0:
-    print("Number is negative")
-else:
-    print("Number is zero")
+# num = int(n);
+# if num > 0:
+#     print("Number is positive")
+# elif num < 0:
+#     print("Number is negative")
+# else:
+#     print("Number is zero")
+
+#loops
+# name = "Fiza"
+# print(name[0])
+
+
+
+#tuples
+
+# coordinates = (10.0, 20.0)
+# print(coordinates[0])
